e570332bef08506da8826acce39221ef86207e00
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2025-04-14 13:41:21 -0300

    as

--- GiovanniPeirone/ejersicio-04.py
+++ GiovanniPeirone/ejersicio-04.py
@@ -5,7 +5,7 @@ os.environ['kivy_gl_backend'] = 'angle_sdl2'
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.uix.button import Button
 
 class MainInterface(BoxLayout):
     def __init__(self, **kwargs):
@@ -14,6 +14,11 @@ class MainInterface(BoxLayout):
         self.texto = Label(text = 'Hola Mundo', font_size=30)
         self.add_widget(self.texto)
         
+
+class PrimeraVentana(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
 
 
 class Main(App):
