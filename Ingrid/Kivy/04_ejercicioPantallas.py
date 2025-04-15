dd8e1205043daa616758f38484fb1ff753964c0c
ingridd20 <132923687+indridd20@users.noreply.github.com>
2025-04-15 11:45:01 -0300

    ejercicios

---
+++ Ingrid/Kivy/04_ejercicioPantallas.py
@@ -0,0 +1,36 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+KV = '''
+ScreenManager:
+    id: screen_manager
+    
+    MDScreen:
+    name: "pantalla1"
+    MDLabel:
+        text: "Pantalla 1"
+        halign: "center"
+    MDFlatButton:
+        text: "ir a pantalla 2"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        on_release: app.change_screen(pantalla2)
+        
+    MDScreen:
+    name: "pantalla2"
+    MDLabel:
+        text: "Pantalla 2"
+        halign: "center"
+    MDFlatButton:
+        text: "Volver a pantalla 1"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        on_release: app.change_screen(pantalla1)
+    '''
+
+class MyApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+    
+    def change_screen(self, screen_name):
+        self.root.current = screen_name
+
+MyApp().run()
